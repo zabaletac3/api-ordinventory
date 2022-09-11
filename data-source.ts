85f9baf31201f9d4b0be7e3911843d3aaedfc442
hfcarrillo <hfcarrillo@indracompany.com>
2022-09-10 23:01:57 -0500

    Se modifica el datasource
    Se cambia el password del datasource y se quita la sincronización a DB

--- data-source.ts
+++ data-source.ts
@@ -1,19 +1,22 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { Comprobante } from "./server/models/comprobant.entity";
-import { DetalleComprobante } from "./server/models/detalle.comprobante.entity";
-import { Employees } from "./server/models/employees.entity";
-import { Products } from "./server/models/products.entity";
-import { User } from "./server/models/user.entity";
+import { DataSource } from 'typeorm';
+
+import { Comprobante } from './server/models/comprobant.entity';
+import { DetalleComprobante } from './server/models/detalle.comprobante.entity';
+import { Employees } from './server/models/employees.entity';
+import { Products } from './server/models/products.entity';
+import { User } from './server/models/user.entity';
+
+import 'reflect-metadata';
+import 'reflect-metadata';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "Zabaleta3#",
+    password: "admin",
     database: "ord_ventory",
-    synchronize: true,
+    //synchronize: false,
     logging: false,
     entities: [User, Products, Employees, Comprobante, DetalleComprobante],
     migrations: [],
